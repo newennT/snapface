a8604b9040cd2f081493e7867865c3715e595162
NewennT <newenn.turbiau@gmail.com>
2025-12-07 22:22:18 +0100

    Passer en SPA

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { FaceSnapListComponent } from './components/face-snap-list/face-snap-list';
+import { LandingPageComponent } from './components/landing-page/landing-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'facesnaps', component: FaceSnapListComponent },
+    { path: '', component: LandingPageComponent}
+    
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FaceSnapListComponent } from "./components/face-snap-list/face-snap-list";
 import { Header } from "./components/header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FaceSnapListComponent, Header],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
