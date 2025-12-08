8a0706a2fc56f60eb7ec9d2a97bc17641b1f404b
NewennT <newenn.turbiau@gmail.com>
2025-12-08 19:36:30 +0100

    Méthodes routerlink

--- src/app/components/landing-page/landing-page.ts
+++ src/app/components/landing-page/landing-page.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
 })
 export class LandingPageComponent {
+
+  constructor(private router: Router) {
+
+  }
+
+  onContinue(){
+    this.router.navigate(['facesnaps']);
+
+  }
 
 }
